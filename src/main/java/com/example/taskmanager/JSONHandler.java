efd220a8c86f7f214afe622a540d2a33b4e38e83
NALEX-0 <nikosalexa2002@gmail.com>
2026-07-31 18:27:59 +0300

    Update Maven config for Java 21 and JavaFX 21 for Apple Silicon support

--- src/main/java/com/example/taskmanager/JSONHandler.java
+++ src/main/java/com/example/taskmanager/JSONHandler.java
@@ -19,7 +19,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
  */
 public class JSONHandler implements Serializable {
     // Fix path accordingly with computer/server
-    // private static final String DATA_DIR = "TaskManager/medialab/";
+    // private static final String DATA_DIR = "TaskManager/src/main/resources/medialab/";
     private static final String DATA_DIR = "C:/dev/code/Java/multimedia_project/TaskManager/src/main/resources/medialab/";
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
